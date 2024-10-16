273208828ab2a48fc81ac49b94eb0c3ff399c153
Jairo Ortega <jl.ortega01072001@gmail.com>
2024-10-17 06:01:51 +0800

    add design admin layout and dashboard

---
+++ resources/js/Components/AdminNavLink.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { computed } from 'vue';
+import { Link } from '@inertiajs/vue3';
+
+const props = defineProps({
+    href: {
+        type: String,
+        required: true,
+    },
+    active: {
+        type: Boolean,
+    },
+});
+
+const classes = computed(() =>
+    props.active
+    //     class="flex items-center p-2 text-base text-gray-900 rounded-lg hover:bg-gray-100 group dark:text-gray-200 dark:hover:bg-gray-700"
+        ? 'flex w-full p-2 text-gray-600 rounded-lg text-base font-medium bg-pink-50 transition duration-150 ease-in-out'
+        : 'flex w-full p-2 text-gray-600 rounded-lg text-base font-medium hover:bg-gray-100 transition duration-150 ease-in-out'
+);
+</script>
+
+<template>
+    <Link :href="href" :class="classes">
+        <slot />
+    </Link>
+</template>
--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -1,18 +1,71 @@
 <script setup>
-import AdminAuthenticatedLayout from '@/Layouts/AdminAuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import AdminAuthenticatedLayout from "@/Layouts/AdminAuthenticatedLayout.vue";
+import { Head } from "@inertiajs/vue3";
 </script>
 
 <template>
     <Head title="Dashboard" />
 
     <AdminAuthenticatedLayout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Admin Dashboard</h2>
+        <template #breadcrumbs>
+            <div class="pb-2">
+                <nav class="flex">
+                    <ol
+                        class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse"
+                    >
+                        <li class="inline-flex items-center">
+                            <a
+                                :href="route('admin.dashboard')"
+                                class="inline-flex items-center text-sm font-medium text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white"
+                            >
+                                <svg
+                                    class="w-4 h-4 me-2.5"  
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="currentColor"
+                                    viewBox="0 0 20 20"
+                                >
+                                    <path
+                                        d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z"
+                                    />
+                                </svg>
+                                Home
+                            </a>
+                        </li>
+                        <li>
+                            <div class="flex items-center">
+                                <svg
+                                    class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 6 10"
+                                >
+                                    <path
+                                        stroke="currentColor"
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="m1 9 4-4-4-4"
+                                    />
+                                </svg>
+                                <span
+                                    class="ms-1 text-sm font-medium text-gray-700 md:ms-2 dark:text-gray-400"
+                                    >Dashboard</span
+                                >
+                            </div>
+                        </li>
+                    </ol>
+                </nav>
+            </div>
         </template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <template #header>
+            <h2 class="font-semibold text-5xl pb-4 text-gray-800 leading-tight">
+                Dashboard
+            </h2>
+        </template>
+
+        <div>
+            <div class="max-w-7xl mx-auto">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">You're logged in!</div>
                 </div>
