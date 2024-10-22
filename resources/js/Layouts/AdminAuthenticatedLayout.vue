bfa0cca58ab9d01ac13e1330ca4d1f3783418551
Jairo Ortega <jl.ortega01072001@gmail.com>
2024-10-23 06:38:40 +0800

    modify component and layout

--- resources/js/Layouts/AdminAuthenticatedLayout.vue
+++ resources/js/Layouts/AdminAuthenticatedLayout.vue
@@ -250,15 +250,15 @@ onBeforeUnmount(() => {
                                             },
                                         ]"
                                         fill="currentColor"
-                                        viewBox="0 0 20 20"
+                                        viewBox="0 0 22 21"
                                         xmlns="http://www.w3.org/2000/svg"
                                     >
                                         <path
-                                            d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"
-                                        ></path>
+                                            d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"
+                                        />
                                         <path
-                                            d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"
-                                        ></path>
+                                            d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"
+                                        />
                                     </svg>
                                     <span class="ml-3">Dashboard</span>
                                 </AdminNavLink>
@@ -447,7 +447,6 @@ onBeforeUnmount(() => {
                 id="main-content"
                 class="relative w-full h-full overflow-y-auto bg-gray-50 lg:ml-64 dark:bg-gray-900"
             >
-
                 <header v-if="$slots.header">
                     <div class="mx-auto">
                         <slot name="header" />
